9b5bea9abf1524098e6e467a822eaa306ff9247f
AG-w <44581470+AG-w@users.noreply.github.com>
2024-04-12 17:16:41 +0800

    Update sd-webui-smea.py

--- scripts/sd-webui-smea.py
+++ scripts/sd-webui-smea.py
@@ -51,6 +51,8 @@ def init():
         ('Euler Dy', sample_euler_dy, ['k_euler'], {}),
         ('Euler Smea', sample_euler_smea, ['k_euler'], {}),
         ('Euler Smea Dy', sample_euler_smea_dy, ['k_euler'], {}),
+        ('Euler Dy koishi-star', sample_euler_dy_og, ['k_euler'], {}),
+        ('Euler Smea Dy koishi-star', sample_euler_smea_dy_og, ['k_euler'], {}),
     ]
 
     samplers_data_smea = [
@@ -63,6 +65,8 @@ def init():
         sample_euler_dy: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
         sample_euler_smea: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
         sample_euler_smea_dy: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
+        sample_euler_dy_og: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
+        sample_euler_smea_dy_og: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
     }
     sd_samplers_kdiffusion.sampler_extra_params = {**sd_samplers_kdiffusion.sampler_extra_params, **sampler_exparams_smea}
 	
@@ -229,6 +233,55 @@ def sample_euler_smea_dy(model, x, sigmas, extra_args=None, callback=None, disab
             elif i % 2 == 1:
                 x_temp = smea_sampling_step(x_2, model, dt_2, sigma_mid, **extra_args)
             x = x_temp - d * dt_1
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
+    return x
+
+@torch.no_grad()
+def sample_euler_dy_og(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
+    extra_args = {} if extra_args is None else extra_args
+    s_in = x.new_ones([x.shape[0]])
+    for i in trange(len(sigmas) - 1, disable=disable):
+        # print(i)
+        # i第一步为0
+        gamma = max(s_churn / (len(sigmas) - 1), 2 ** 0.5 - 1) if s_tmin <= sigmas[i] <= s_tmax else 0.
+        eps = torch.randn_like(x) * s_noise
+        sigma_hat = sigmas[i] * (gamma + 1)
+        # print(sigma_hat)
+        dt = sigmas[i + 1] - sigma_hat
+        if gamma > 0:
+            x = x - eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
+        denoised = model(x, sigma_hat * s_in, **extra_args)
+        d = sampling.to_d(x, sigma_hat, denoised)
+        if sigmas[i + 1] > 0:
+            if i // 2 == 1:
+                x = dy_sampling_step(x, model, dt, sigma_hat, **extra_args)
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
+        # Euler method
+        x = x + d * dt
+    return x
+
+@torch.no_grad()
+def sample_euler_smea_dy_og(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
+    extra_args = {} if extra_args is None else extra_args
+    s_in = x.new_ones([x.shape[0]])
+    for i in trange(len(sigmas) - 1, disable=disable):
+        gamma = max(s_churn / (len(sigmas) - 1), 2 ** 0.5 - 1) if s_tmin <= sigmas[i] <= s_tmax else 0.
+        eps = torch.randn_like(x) * s_noise
+        sigma_hat = sigmas[i] * (gamma + 1)
+        dt = sigmas[i + 1] - sigma_hat
+        if gamma > 0:
+            x = x - eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
+        denoised = model(x, sigma_hat * s_in, **extra_args)
+        d = sampling.to_d(x, sigma_hat, denoised)
+        # Euler method
+        x = x + d * dt
+        if sigmas[i + 1] > 0:
+            if i + 1 // 2 == 1:
+                x = dy_sampling_step(x, model, dt, sigma_hat, **extra_args)
+            if i + 1 // 2 == 0:
+                x = smea_sampling_step(x, model, dt, sigma_hat, **extra_args)
         if callback is not None:
             callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
     return x
