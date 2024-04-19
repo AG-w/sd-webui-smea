0ff6e07f7301d003bfb62ce2e13ce31cfd8ec254
AG-w <44581470+AG-w@users.noreply.github.com>
2024-04-19 21:40:53 +0800

    Update sd-webui-smea.py

--- scripts/sd-webui-smea.py
+++ scripts/sd-webui-smea.py
@@ -544,8 +544,8 @@ def sample_euler_smea_multi_ds(model, x, sigmas, extra_args=None, callback=None,
                 denoised_2b = smea_sampling_step_denoised(x_2, model, sigma_mid, 1 + scale * 0.15, **extra_args)
                 denoised_2 = denoised_2a * 0.4 + denoised_2b * 0.6
             else:
-                denoised_2b = smea_sampling_step_denoised(x_2, model, sigma_mid, 1 + scale * 0.03, True, **extra_args)
-                denoised_2c = model(x_2, sigma_mid * s_in, **extra_args)
+                denoised_2b = smea_sampling_step_denoised(x_2, model, sigma_mid, 1 + scale * 0.05, True, **extra_args)
+                denoised_2c = smea_sampling_step_denoised(x_2, model, sigma_mid, 1 + scale * 0.1, **extra_args)
                 denoised_2 = denoised_2b * 0.6 + denoised_2c * 0.4
             d_2 = to_d(x_2, sigma_mid, denoised_2)
             x = x + d_2 * dt_2
