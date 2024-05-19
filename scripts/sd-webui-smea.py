bd11008bf1af048e1f9b10227b3b613954335306
AG-w <44581470+AG-w@users.noreply.github.com>
2024-05-19 22:23:24 +0800

    Update sd-webui-smea.py

--- scripts/sd-webui-smea.py
+++ scripts/sd-webui-smea.py
@@ -48,7 +48,8 @@ def init():
 
     samplers_smea = [
         ('Euler Max', sample_euler_max, ['k_euler'], {}),
-        ('Euler Max2', sample_euler_max2, ['k_euler'], {}), 
+        ('Euler Max2', sample_euler_max2, ['k_euler'], {}),
+        ('Euler Max2b', sample_euler_max2b, ['k_euler'], {}),
         ('Euler Dy', sample_euler_dy, ['k_euler'], {}),
         ('Euler Smea', sample_euler_smea, ['k_euler'], {}),
         ('Euler Smea Dy', sample_euler_smea_dy, ['k_euler'], {}),		
@@ -83,6 +84,7 @@ def init():
     sampler_exparams_smea = {
         sample_euler_max: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
         sample_euler_max2: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
+        sample_euler_max2b: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
         sample_euler_dy: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
         sample_euler_smea: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
         sample_euler_smea_dy: ['s_churn', 's_tmin', 's_tmax', 's_noise'],
@@ -219,6 +221,25 @@ def sample_euler_max2(model, x, sigmas, extra_args=None, callback=None, disable=
         dt = sigmas[i + 1] - sigma_hat
         # Euler method
         x = x + (math.cos(1.05 * i + 1)/(1.1 * i + 1.5) + 1) * d * dt
+    return x
+
+@torch.no_grad()
+def sample_euler_max2b(model, x, sigmas, extra_args=None, callback=None, disable=None, s_churn=0., s_tmin=0., s_tmax=float('inf'), s_noise=1.):
+    extra_args = {} if extra_args is None else extra_args
+    s_in = x.new_ones([x.shape[0]])
+    for i in trange(len(sigmas) - 1, disable=disable):
+        gamma = max(s_churn / (len(sigmas) - 1), 2 ** 0.5 - 1) if s_tmin <= sigmas[i] <= s_tmax else 0.
+        eps = k_diffusion.sampling.torch.randn_like(x) * s_noise
+        sigma_hat = sigmas[i] * (gamma + 1)
+        if gamma > 0:
+            x = x - eps * (sigma_hat ** 2 - sigmas[i] ** 2) ** 0.5
+        denoised = model(x, sigma_hat * s_in, **extra_args)
+        d = to_d(x, sigma_hat, denoised)
+        if callback is not None:
+            callback({'x': x, 'i': i, 'sigma': sigmas[i], 'sigma_hat': sigma_hat, 'denoised': denoised})
+        dt = sigmas[i + 1] - sigma_hat
+        # Euler method
+        x = x + (math.cos(1.05 * i + 1.1)/(1.25 * i + 1.5) + 1) * d * dt
     return x
 
 @torch.no_grad()
